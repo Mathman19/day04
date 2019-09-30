ad96c2435719414b1ec35315d084d33bec553999
Muhammad Akbar <akbarprima95@gmail.com>
2019-09-30 10:44:05 +0700

    console application

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace caDay04 {
+    class Program {
+        static void Main(string[] args) {
+            //perintah untuk cetak hasil
+            Console.WriteLine("Selamat Datang");
+
+            //trik nunggu input agar tidak close
+            Console.ReadLine();
+        }
+    }
+}
